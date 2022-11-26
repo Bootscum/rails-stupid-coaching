222f19afe080d571bc9c62d10880a9328c496632
john stowers <stowers.elliot@gmail.com>
2022-11-26 14:52:41 +0800

    The Coach may wag is tongue, but he'll soon wear out his lung. I got the Challenge Complete, but on quality I won't compete.

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    # @answer = answer
+
+      # if params[:answer].include?("?")
+      #   return "Silly question, get dressed and go to work!"
+      # elsif params[:answer].downcase == "i am going to work right now!"
+      #   return ""
+      # else
+      #   return "I don't care, get dressed and go to work!"
+      # end
+
+
+  end
+end
